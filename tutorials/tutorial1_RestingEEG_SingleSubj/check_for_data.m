b8d7aba57dfa70a27b059c2b1f4f223cba69bb4a
Luke Tait (Metabolism and Systems Research) <taitlz@COLMDS-D1LAMD6R.local>
2021-08-27 17:10:03 +0100

    Add tutorial 1

---
+++ tutorials/tutorial1_RestingEEG_SingleSubj/check_for_data.m
@@ -0,0 +1,34 @@
+% Get location of the toolbox and specify data directory
+toolbox_path = microstate.functions.toolbox_path ; % location where the toolbox is installed
+datadir = fullfile(fileparts(toolbox_path),'example','example1_RestingEEG_SingleSubj') ; % path to folder containing the data
+addpath(datadir) % add this to the path
+
+% Check if the data is in the data directory
+if ~exist(fullfile(datadir,'example1_data.mat'),'file')
+    answer = questdlg('Data for example 1 has not been downloaded. Download now?',...
+        'Download data?',...
+        'Yes','No','Yes') ;
+
+    switch answer
+        case 'Yes'
+
+            % Install the data
+            datafile = fullfile(datadir,'example1_data.mat') ;
+            url = ['https://github.com/plus-microstate/toolbox/raw/master/' datafile] ; 
+            url = replace(url,filesep,'/') ; 
+            url = replace(url,'+','%2B') ; 
+            webopts = weboptions('Timeout',120);
+            websave(datafile,url,webopts) ; 
+            clear datafile url webopts answer
+
+        case 'No'
+
+            % Finish running the data
+            errordlg('Cannot run tutorial without downloading data','Cancel tutorial')
+            clear answer
+            return
+
+    end
+
+end
+    
